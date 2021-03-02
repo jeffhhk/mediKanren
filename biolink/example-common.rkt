00ba20b733e84bfa72b69ba023d6ed4e7dfad415
Jeff Henrikson <79888432+jeffhhk@users.noreply.github.com>
2021-03-02 08:50:29 -0800

    ciprep: refactor: example-common.rkt: expose queries for testing using module+ main

--- biolink/example-common.rkt
+++ biolink/example-common.rkt
@@ -4,48 +4,84 @@
   (all-defined-out))
 (require "common.rkt")
 
+  (define (query1)
+    (run* (c) (categoryo c)))
+
+  (define (query2)
+    (run* (p) (predicateo p)))
+
+  (define (query3)
+  (run 10 (c) (concepto c))
+  )
+
+  (define (query4)
+  (run 10 (e) (edgeo e))
+  )
+
+  (define (query5)
+  (run* (c) (~name*-concepto (list "imatin" "400") c))
+  )
+
+  (define (query6)
+  (run* (c)
+    (~cui*-concepto
+      ;; Match CURIEs that match any of the listed fragments.
+      (list "UMLS:C0004096" "DOID:2841" "HP:0002099" "MONDO:0004979")
+      c))
+  )
+
+  (define (query7)
+  (run* (xref c)
+    (membero xref '("DOID:2841" "HP:0002099" "MONDO:0004979"))
+    (xref-concepto xref c))
+  )
+
+  (define (query8)
+  (run 3 (pmid e) (pmid-edgeo pmid      e))
+  )
+
+  (define (query9 v)
+  (run*       (e) (pmid-edgeo v e))
+  )
+
+(module+ main
 (displayln "all categories:")
-(pretty-print (run* (c) (categoryo c)))
+(pretty-print (query1))
 
 (newline)
 (displayln "all predicates:")
-(pretty-print (run* (p) (predicateo p)))
+(pretty-print (query2))
 
 (newline)
 (displayln "some concepts:")
-(pretty-print (run 10 (c) (concepto c)))
+(pretty-print (query3))
 
 (newline)
 (displayln "some edges:")
-(pretty-print (run 10 (e) (edgeo e)))
+(pretty-print (query4))
 
 (newline)
 (displayln "fuzzy name search:")
 (time (pretty-print
         ;; Match names that include all listed fragments.
-        (run* (c) (~name*-concepto (list "imatin" "400") c))))
+        (query5)))
 
 (newline)
 (displayln "CURIE search:")
 (time (pretty-print
-        (run* (c)
-          (~cui*-concepto
-            ;; Match CURIEs that match any of the listed fragments.
-            (list "UMLS:C0004096" "DOID:2841" "HP:0002099" "MONDO:0004979")
-            c))))
+        (query6)))
 
 (newline)
 (displayln "xref search:")
 (time (pretty-print
-        (run* (xref c)
-          (membero xref '("DOID:2841" "HP:0002099" "MONDO:0004979"))
-          (xref-concepto xref c))))
+        (query7)))
 
 (newline)
 (displayln "finding pubmed ids and associated edges:")
-(time (pretty-print (run 3 (pmid e) (pmid-edgeo pmid      e))))
+(time (pretty-print (query8)))
 (displayln "associating specific pubmed ids with edges:")
-(time (pretty-print (run*       (e) (pmid-edgeo "1000085" e))))
-(time (pretty-print (run*       (e) (pmid-edgeo "1000018" e))))
-(time (pretty-print (run*       (e) (pmid-edgeo "10000"   e))))
-(time (pretty-print (run*       (e) (pmid-edgeo "999999"  e))))
+(time (pretty-print (query9 "1000085")))
+(time (pretty-print (query9 "1000018")))
+(time (pretty-print (query9 "10000")))
+(time (pretty-print (query9 "999999")))
+)
